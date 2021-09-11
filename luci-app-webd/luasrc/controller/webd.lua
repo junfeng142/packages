c17d57c44f327a30db84b0478b0754ccc9e99980
javonca <junfeng142@gmail.com>
2021-09-11 08:29:45 +0800

    update my packages

---
+++ luci-app-webd/luasrc/controller/webd.lua
@@ -0,0 +1,19 @@
+module("luci.controller.webd", package.seeall)
+
+function index()
+	if not nixio.fs.access("/etc/config/webd") then
+		return
+    end
+    
+    entry({"admin", "nas"}, firstchild(), "NAS", 44).dependent = false
+	entry({"admin", "nas", "webd"}, cbi("webd"), _("Web Filemanage"), 200).dependent=false
+	entry({"admin", "nas", "webd","status"},call("act_status")).leaf=true
+
+end
+
+function act_status()
+    local e={}
+    e.running=luci.sys.call("pgrep webd >/dev/null")==0
+    luci.http.prepare_content("application/json")
+    luci.http.write_json(e)
+end
